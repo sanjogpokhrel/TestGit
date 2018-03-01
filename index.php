4cb6ac25f9d00ff4539fc36c809c992e4201e870
sanjogpokhrel <sanjogpokhrel@outlook.com>
2018-03-01 01:20:36 -0600

    first commint

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+  class Post{
+
+  	public function_set($name, $value){
+  		echo 'Setting' .$name. 'to <strong>'. $value
+  		$this->name = $value;
+  	}
+  }
+
+$post = new Post;
+$post->name = 'Testing';
+
+?>
